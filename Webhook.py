c6fee6b353b5b2adaa624af919da4414734b6e28
Cyrus <80195151+CyrusSE@users.noreply.github.com>
2021-09-14 10:06:42 +0700

    Update Webhook.py

--- Webhook.py
+++ Webhook.py
@@ -3,21 +3,25 @@ import os
 from os import system
 
 os.system('cls')
-
+print('Simple Discord webhook sender in Python\n')
 a = input('Enter Webhook : ')
+b = input('Enter Avatar : ')
+c = input('Enter Username: ')
 
 def webhook():
     while True:
         try:
             webhook = (a)
+            avatar = (b)
+            username = (c)
             message = input("Enter Message: ")
-            data = requests.post(webhook, json={'content': message})
+            data = requests.post(webhook, json={'content': message, 'avatar_url': avatar, "username": username})
 
             if data.status_code == 204:
                 print('\n' 'Sent : ' + message)
                 print('To : ' + a, '\n')
             else:
-                print("\nInvalid webhook or Deleted \n-> " + webhook)
+                print("\nWebhook Error Deleted\n-> " + webhook)
                 break
         except:
             break
